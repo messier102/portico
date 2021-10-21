7e5649e6eaf7d55346ce625a43f729942807a1d3
messier102 <messier102@yandex.ru>
2021-10-21 11:31:30 +0700

    Fix incorrect declaration

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,4 +1,4 @@
-/// <reference types="svelte" />
+/// <reference types="@sveltejs/kit" />
 declare namespace svelte.JSX {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     interface HTMLAttributes<T> {
