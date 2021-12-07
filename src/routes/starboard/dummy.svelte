b38c9ec176d772547973a22e5a0a191a810d5c20
messier102 <messier102@yandex.ru>
2021-12-07 14:34:04 +0700

    Remove the dummy source

--- src/routes/starboard/dummy.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import ImageGallery from "$lib/components/ImageGallery.svelte";
-    import { HentsuDummySource } from "$lib/model/sources/hentsu/dummy";
-
-    const imageSource = new HentsuDummySource(document.location.toString());
-</script>
-
-<ImageGallery {imageSource} />
