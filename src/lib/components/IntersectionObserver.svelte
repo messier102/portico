85c0d8456e84a1af34fb9148abd44464b175c088
messier102 <messier102@yandex.ru>
2022-03-24 16:01:51 +0700

    Implement a loading indicator/trigger

---
+++ src/lib/components/IntersectionObserver.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { createEventDispatcher, onMount } from "svelte";
+
+    const dispatch = createEventDispatcher();
+
+    let target: HTMLElement;
+
+    let observer = new IntersectionObserver(([entry]) => {
+        dispatch(entry.isIntersecting ? "enter" : "exit");
+    });
+
+    onMount(() => observer.observe(target));
+</script>
+
+<div bind:this={target}>
+    <slot />
+</div>
